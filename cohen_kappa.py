d361624fc77580ad8fab84597acc9e3e528518c5
ChanHT <chanhts323@gmail.com>
2022-09-15 22:35:49 +0800

    Add files via upload

--- cohen_kappa.py
+++ cohen_kappa.py
@@ -13,7 +13,11 @@ Args:
 """
 import pandas as pd
 from sklearn.metrics import cohen_kappa_score
-
+import numpy as np
+import itertools
+import seaborn as sns
+import matplotlib.pyplot as plt
+rater_num = 5
 def count_cohen_kappa(y1,y2):
     kappa_value = cohen_kappa_score(
         y1,
@@ -23,8 +27,6 @@ def count_cohen_kappa(y1,y2):
         sample_weight = None
     )
     return kappa_value
-
-
 def create_labeler_set(path):
     df = pd.read_csv(path,encoding = 'UTF-8')
     labeler_a = []
@@ -33,13 +35,52 @@ def create_labeler_set(path):
         labeler_a.append(df['A'][i])
         labeler_b.append(df['B'][i])
     return labeler_a, labeler_b
+
+def Calculate_Cohen_Kappa_more_raters(raters):
+    data = np.zeros((len(raters), len(raters)))
+    for j, k in list(itertools.combinations(range(len(raters)), r=2)):
+        data[j, k] = cohen_kappa_score(raters[j], raters[k])
+    fig, ax = plt.subplots(figsize=(10,8)) 
+    sns.heatmap(
+        data, 
+        mask=np.tri(len(raters)),
+        annot=True, 
+        linewidths=0.5,
+        vmin=0, 
+        vmax=1,
+        xticklabels=[f"Dermatologist {k + 1}" for k in range(len(raters))],
+        yticklabels=[f"Dermatologist {k + 1}" for k in range(len(raters))],
+        cmap="YlGnBu",
+        ax=ax
+    )
+    plt.xticks(rotation=30) 
+    plt.yticks(rotation=30)
+    plt.savefig('result.png')
+    plt.show()
     
 
+def create_raters_set(path):
+    df = pd.read_csv(path,encoding = 'UTF-8')
+    rater_A, rater_B, rater_C, rater_D, rater_E = [],[],[],[],[]
+    for i in range(len(df['Order'])):
+        rater_A.append(df['A'][i])
+        rater_B.append(df['B'][i])
+        rater_C.append(df['C'][i])
+        rater_D.append(df['D'][i])
+        rater_E.append(df['E'][i])
+    raters_table = [rater_A, rater_B, rater_C, rater_D, rater_E]
+    return raters_table  
+
 if __name__=='__main__':
-    path_csv = r'label_2022_0827.csv'
+    path_csv = r'annoaction_2022_0827.csv'
     labeler_a, labeler_b = create_labeler_set(path_csv)
+    raters_table = create_raters_set(path_csv)
     print(labeler_a)
     print()
     print(labeler_b)
     kappa_result = count_cohen_kappa(labeler_a,labeler_b)
     print(kappa_result)
+    print()
+    #print(raters_table)
+    Calculate_Cohen_Kappa_more_raters(raters_table)
+
